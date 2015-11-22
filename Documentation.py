16f3ac433dab548daa21775fd6a1b8c97940c562
Joseph Molina <josephmolina3388@gmail.com>
2015-11-22 00:45:39 -0800

    Deleting duplicate copy of documenation
    This file contained duplication documentation that is included in the main project file.

--- Documentation.py
+++
@@ -1,5 +0,0 @@
-#filename: Semester2.py
-#Author: Joseph Molina
-#Description: Program run a story based on the input of pictures the user selects
-#Version: 1
-#Compiler: JES(Jython Enviornment for Students)
